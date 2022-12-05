37257fa8352ebe1d9f36b9c8a2bc92b80674d568
Oliver Melgrove <38347766+melgrove@users.noreply.github.com>
2022-12-04 18:06:57 -0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -67,7 +67,7 @@
 	<div style="width: 100%">
 		{#if showInfo}
 			<div class="infopanel">
-				This is a search engine that Oliver Melgrove wrote for the final project of Statistics 101C taught by Shirong Xu at UCLA. It uses a dataset of Yelp reviews from Santa Barbara to make a prediction of what restaurants someone would like to see given a search query. The reviews have been encoded using GloVe pre-trained twitter embeddings, and the model has been trained using XGBoost. The link to the paper can be found <a target="_blank" href="/101C-1.pdf">here</a>.
+				This is a search engine that Oliver Melgrove wrote for the final project of Statistics 101C taught by Shirong Xu at UCLA. It uses a dataset of Yelp reviews from Santa Barbara to make a prediction of what restaurants someone would like to see given a search query. The reviews have been encoded using GloVe pre-trained twitter embeddings, and the model has been trained using XGBoost. The link to the paper can be found <a style="color: blue" target="_blank" href="/101C-1.pdf">here</a>.
 			</div>
 			<div on:click={() => showInfo = false} class="showinfo">
 				HIDE INFO
